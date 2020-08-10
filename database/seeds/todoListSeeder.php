464e55e7a4eb7059c2aa6bc867915cc0adac6526
Modixe <Aleksey_97@inbox.ru>
2020-08-10 17:42:46 +0700

    1.0

---
+++ database/seeds/todoListSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class todoListSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('TodoList')->insert([
+
+            'name_list' => str::random(10)
+
+
+        ]);
+    }
+}
