aaeb3013bb89021626f116a41f4ec008548334ef
Modixe <Aleksey_97@inbox.ru>
2020-08-20 00:33:04 +0700

    Рефактор кода 1.2

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -17,12 +17,10 @@ class TaskController extends Controller
      * @param TaskList $task_list
      * @return string
      */
-
     public function index(TaskList $task_list) {
         $all_lists = Task::where('list_id', $task_list->id)->get();
         return $all_lists;
     }
-
     /**
      * Сохраните вновь созданный ресурс в хранилище.
      *
@@ -30,7 +28,6 @@ class TaskController extends Controller
      * @return Response
      *
      */
-
     public function store(Request $request) {
         $create_task = Task::create ([
             'list_id' => $request->task_list->id,
@@ -49,7 +46,6 @@ class TaskController extends Controller
      * @param Task $task
      * @return Task|JsonResponse|object
      */
-
     public function show(TaskList $taskList,Task $task) {
         $lists= Task::findOrFail($task->id);
         return $lists;
@@ -62,7 +58,6 @@ class TaskController extends Controller
      * @param Task $task
      * @return void
      */
-
     public function update(Request $request, Task $task) {
         $update_task = Task::findOrFail($task->id);
         $update_task->update ($request->only(
@@ -79,7 +74,6 @@ class TaskController extends Controller
      * @param Task $task
      * @return void
      */
-
     public function destroy(Task $task) {
         Task::findOrFail($task->id)->delete();
     }
