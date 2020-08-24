9efd023b9d27cab9ffed4eca20673c6499381dee
Modixe <Aleksey_97@inbox.ru>
2020-08-24 22:32:02 +0700

    codestyle

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Resources\TaskCollection as TaskResource;
 
-
 Route::namespace('Api')->group(function (){
     Route::apiResource('task_lists', 'TaskListController');
     Route::apiResource('task_lists.tasks', 'TaskController');
@@ -14,7 +13,6 @@ Route::namespace('Api')->group(function (){
 
 Route::middleware('auth:api')->get('/user', function (Request $request){
     return $request->user();
-
 });
 
 
